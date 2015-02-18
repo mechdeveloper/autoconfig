ff79a17d540aa4320897718af40189715a8f2d4e
Miguel Rodriguez <miguelachoo@gmail.com>
2015-02-18 09:10:05 -0430

    RVM installation files are now using new gpg command.

--- RVM.sh
+++ RVM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+wget https://raw.githubusercontent.com/mechdeveloper/autoconfig/master/RVM.gpg.sh && source RVM.gpg.sh
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 source /usr/local/rvm/scripts/rvm
--- rails-savon-rspec.RVM.RHEL.sh
+++ rails-savon-rspec.RVM.RHEL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo yum -y install nodejs sqlite-devel
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+wget https://raw.githubusercontent.com/mechdeveloper/autoconfig/master/RVM.gpg.sh && source RVM.gpg.sh
 \curl -sSL https://get.rvm.io | bash -s stable --ruby --gems=rails,savon,rspec
 source /usr/local/rvm/scripts/rvm
