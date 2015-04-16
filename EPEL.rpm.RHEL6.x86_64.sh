cea310eb1e96e3b87bdc462312ab2e6fb7aa8453
Miguel Rodriguez <miguel@mechdeveloper.com>
2015-04-16 13:23:50 -0430

    new EPEL installation scripts were added.

---
+++ EPEL.rpm.RHEL6.x86_64.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -ivh epel-release-6-8.noarch.rpm
---
+++ EPEL.rpm.RHEL7.x86_64.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+rpm -ivh epel-release-7-5.noarch.rpm
