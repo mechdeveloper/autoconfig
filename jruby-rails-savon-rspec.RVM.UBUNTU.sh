bb9cf24a0b7386ac7ad634f306a3345cfa7a5cd3
Miguel Rodriguez <miguelachoo@gmail.com>
2015-10-12 15:55:19 -0430

    All the RVM installation scripts will no longer run node.js installation

--- jruby-rails-savon-rspec.RVM.UBUNTU.sh
+++ jruby-rails-savon-rspec.RVM.UBUNTU.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/mechdeveloper/autoconfig/master/node.UBUNTU.sh && source node.UBUNTU.sh
 wget https://raw.githubusercontent.com/mechdeveloper/autoconfig/master/RVM.gpg.sh && source RVM.gpg.sh
 \curl -sSL https://get.rvm.io | bash -s stable --ruby=jruby --gems=puma,rails,savon,rspec
 source /usr/local/rvm/scripts/rvm
