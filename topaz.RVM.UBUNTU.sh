9d20aa58da10e8d0b2395cee4261a192ff045a0f
Miguel Rodriguez <miguelachoo@gmail.com>
2016-11-30 15:10:31 -0400

    adding RVM and Ruby with Topaz interpreter.

---
+++ topaz.RVM.UBUNTU.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/mechdeveloper/autoconfig/master/RVM.gpg.sh && source RVM.gpg.sh
+\curl -sSL https://get.rvm.io | bash -s stable --ruby=topaz
+source /usr/local/rvm/scripts/rvm
